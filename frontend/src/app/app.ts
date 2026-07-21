26348ba89f30f746ff45fd917d9b43c2e762c9c7
Alvaro Rivera <alvaro.regulo.rivera@gmail.com>
2026-07-21 16:44:04 -0400

    feat(frontend): integra token jwt real y nueva ruta en solicitud de laboratorios

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -19,13 +19,19 @@ export class App {
     this.labUrl.set(null);
 
     const payload = {
-      user_id: "2757773f-58d7-4a2f-af05-d224be746aee",
+      user_id: "f9ad3bb1-f05a-4443-ba73-28a9db44ee60",
       template_id: "1281b9f1-3d25-4d34-b18d-a8c9faab20e9",
       ram_limit_mb: 256,
-      user_email: "edwin@uab.edu.bo"
+      user_email: "regulo.rivera@uab.edu.bo"
     };
 
-    this.http.post<any>('http://localhost:3000/labs/start', payload).subscribe({
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJlZ3Vsby5yaXZlcmFAdWFiLmVkdS5ibyIsImV4cCI6MTc4NDc0NTg4Nywicm9sZSI6InN0dWRlbnQiLCJ1c2VyX2lkIjoiZjlhZDNiYjEtZjA1YS00NDQzLWJhNzMtMjhhOWRiNDRlZTYwIn0.vH2ZPeiCJNOhnYUsShdXw7K9E17fw60XX1vx_zNExsg";
+
+    this.http.post<any>('http://localhost:3000/api/v1/labs/start', payload, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    }).subscribe({
       next: (res) => {
         const prefix = payload.user_email.split('@')[0];
         this.labUrl.set(`http://${prefix}-lab.solv.uab.edu.bo`);
